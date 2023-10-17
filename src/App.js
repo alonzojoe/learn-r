f6322265bbc4a92c0d948f30e93d32a52d9c898d
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-10-17 17:02:41 +0800

    Started Component Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurane', amount: 294.56, date: new Date(2023, 10, 17) },
+    { title: 'House Loan', amount: 525.25, date: new Date(2023, 10, 18) },
+    { title: 'Monthly Expenses', amount: 1000, date: new Date(2023, 10, 31) },
+    { title: 'Allowance', amount: 1000, date: new Date(2023, 10, 31) },
+  ]
+
   return (
     <div>
       <h2>Let's get started</h2>
       <p>This is React js</p>
-      <ExpenseItem />
+
+      { expenses.map((item) => {
+        return <ExpenseItem />
+      }) }
+      
     </div>
   );
 }
