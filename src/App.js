600b1deadfc4a4b4c966a8f3f75adb9320b67879
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-10-18 15:59:57 +0800

    JSX Review

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurane",
+      amount: 294.56,
+      date: new Date(2023, 11, 17),
+    },
+    {
+      id: "e2",
+      title: "House Loan",
+      amount: 525.25,
+      date: new Date(2023, 9, 18),
+    },
+    {
+      id: "e3",
+      title: "Monthly Expenses",
+      amount: 1000,
+      date: new Date(2023, 10, 31),
+    },
+    {
+      id: "e4",
+      title: "Allowance",
+      amount: 1000,
+      date: new Date(2023, 10, 31),
+    },
+  ];
 
-
-  return (
-    <div>
-      <h2>Let's get started</h2>
-      <Expenses />
-    </div>
+  return React.createElement(
+    'div', 
+    {}, 
+    React.createElement('h2', {}, 'Lets get Started'),
+    React.createElement(Expenses, {expense: expenses})
   );
+
+  // return (
+  //   <div>
+  //     <h2>Let's get started</h2>
+  //     <Expenses expense={expenses}/>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,43 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import './Expenses.css'
-function Expenses() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurane",
-      amount: 294.56,
-      date: new Date(2023, 11, 17),
-    },
-    {
-      id: "e2",
-      title: "House Loan",
-      amount: 525.25,
-      date: new Date(2023, 9, 18),
-    },
-    {
-      id: "e3",
-      title: "Monthly Expenses",
-      amount: 1000,
-      date: new Date(2023, 10, 31),
-    },
-    {
-      id: "e4",
-      title: "Allowance",
-      amount: 1000,
-      date: new Date(2023, 10, 31),
-    },
-  ];
+function Expenses(props) {
+    console.log(props.expense)
 
   return (
     <Card className="expenses">
-      <ExpenseItem expenses={expenses[0]}></ExpenseItem>
+      <ExpenseItem expenses={props.expense[0]}></ExpenseItem>
 
-      <ExpenseItem expenses={expenses[1]}></ExpenseItem>
+      <ExpenseItem expenses={props.expense[1]}></ExpenseItem>
 
-      <ExpenseItem expenses={expenses[2]}></ExpenseItem>
+      <ExpenseItem expenses={props.expense[2]}></ExpenseItem>
 
-      <ExpenseItem expenses={expenses[3]}></ExpenseItem>
+      <ExpenseItem expenses={props.expense[3]}></ExpenseItem>
     </Card>
   );
 }
