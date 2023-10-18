7b73c39636fba575037d8e706d28d75a80cfb1aa
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-10-18 08:32:00 +0800

    Component Props

--- src/App.js
+++ src/App.js
@@ -2,21 +2,64 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { title: 'Car Insurane', amount: 294.56, date: new Date(2023, 10, 17) },
-    { title: 'House Loan', amount: 525.25, date: new Date(2023, 10, 18) },
-    { title: 'Monthly Expenses', amount: 1000, date: new Date(2023, 10, 31) },
-    { title: 'Allowance', amount: 1000, date: new Date(2023, 10, 31) },
-  ]
+    {
+      id: "e1",
+      title: "Car Insurane",
+      amount: 294.56,
+      date: new Date(2023, 10, 17),
+    },
+    {
+      id: "e2",
+      title: "House Loan",
+      amount: 525.25,
+      date: new Date(2023, 10, 18),
+    },
+    {
+      id: "e3",
+      title: "Monthly Expenses",
+      amount: 1000,
+      date: new Date(2023, 10, 31),
+    },
+    {
+      id: "e4",
+      title: "Allowance",
+      amount: 1000,
+      date: new Date(2023, 10, 31),
+    },
+  ];
 
   return (
     <div>
       <h2>Let's get started</h2>
       <p>This is React js</p>
 
-      { expenses.map((item) => {
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+
+      {/* { expenses.map((item) => {
         return <ExpenseItem />
-      }) }
-      
+      }) } */}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'
 import moment from 'moment'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   const expenseDate = new Date(2023, 10, 17)
   const expenseTitle = "Car Insurance"
   const expenseAmount = 250.05
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div><h2>{expenseTitle}</h2>
+      <div>{props.date.toISOString()}</div><h2>{props.title}</h2>
       <div className="expense-item__description">
-        <div className="expense-item__price">$ {expenseAmount}</div>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
