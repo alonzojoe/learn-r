d0ded53e5963e9f908ff03cbb85961bdc8a66619
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-11-02 10:21:38 +0800

    Added Chart

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,20 +1,20 @@
-
-import React, { useState } from 'react'
-import ExpenseItem from './ExpenseItem';
-import './ExpensesList.css'
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
 
 const ExpenseList = (props) => {
-    console.log(props.expenses.length)
-    if (props.expenses.length === 0) {
-        return <h2 className='expenses-list__fallback'>No Expenses Found</h2>
-    }
-    
+  console.log(props.expenses.length);
+  if (props.expenses.length === 0) {
+    return <h2 className="expenses-list__fallback">No Expenses Found</h2>;
+  }
 
-    return (<ul className='expense-list'>
-        {props.expenses.map((expense) => (
+  return (
+    <ul className="expense-list">
+      {props.expenses.map((expense) => (
         <ExpenseItem key={expense.id} expenses={expense} />
       ))}
-    </ul>)
-} 
+    </ul>
+  );
+};
 
-export default ExpenseList; 
+export default ExpenseList;
